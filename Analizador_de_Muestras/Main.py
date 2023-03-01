07592d6f2b848ee9e117b3a80af3a428f5c0a055
kevin95all <58482652+kevin95all@users.noreply.github.com>
2023-03-01 12:50:43 -0600

    Menu actualizado y modulo TDA terminado

--- Analizador_de_Muestras/Main.py
+++ Analizador_de_Muestras/Main.py
@@ -12,19 +12,21 @@ class Main:
         self.ruta = ''  # -----> Guarda la dirección del archivo XML
         self.op = ''  # -----> Guarda la opción seleccionada del menú
         self.salir = False  # -----> Variable para finalizar el programa
+        self.muestra_seleccionada = False  # -----> variable para seleccionar muestra
         self.archivo = XML()
 
     def menu_principal(self):  # -----> Metodo para mostrar el menu principal
-        while not self.salir:
+        while not self.salir:  # -----> Mientras self.salir sea = False...
             print('                                                       ')
             print(Fore.GREEN + '┌------------ MENU PRINCIPAL ------------┐')
             print(Fore.GREEN + '|                                        |')
             print(Fore.GREEN + '|        1)   Cargar archivo             |')
-            print(Fore.GREEN + '|        2)   Seleccionar paciente       |')
-            print(Fore.GREEN + '|        3)   Mostrar celula             |')
-            print(Fore.GREEN + '|        4)   Analizar paciente          |')
-            print(Fore.GREEN + '|        5)   Generar reporte            |')
-            print(Fore.GREEN + '|        6)   Salir                      |')
+            print(Fore.GREEN + '|        2)   Seleccionar muestra        |')
+            print(Fore.GREEN + '|        3)   Graficar muestra           |')
+            print(Fore.GREEN + '|        4)   Colocar organismo          |')
+            print(Fore.GREEN + '|        5)   Analizar muestra           |')
+            print(Fore.GREEN + '|        6)   Generar XML                |')
+            print(Fore.GREEN + '|        7)   Finalizar programa         |')
             print(Fore.GREEN + '|                                        |')
             print(Fore.GREEN + '└----------------------------------------┘')
             print('                                                       ')
@@ -34,14 +36,16 @@ class Main:
             if self.op == '1':
                 self.cargar_archivo()
             elif self.op == '2':
-                pass
+                self.seleccionar_muestra()
             elif self.op == '3':
-                pass
+                self.graficar_muestra()
             elif self.op == '4':
-                pass
+                self.colocar_organismo()
             elif self.op == '5':
-                pass
+                self.analizar_muestra()
             elif self.op == '6':
+                self.generar_xml()
+            elif self.op == '7':
                 self.finalizar()
             else:
                 print('                               ')
@@ -65,9 +69,24 @@ class Main:
             print(Fore.RED + '--> No se cargo ningun archivo')
         else:
             self.archivo.leer_xml(self.ruta)
-            print('                                          ')
-            print(Fore.GREEN + '--> Archivo cargado con exito')
+            print('                                         ')
+            print(Fore.CYAN + '--> Archivo cargado con exito')
         ventana.mainloop()
+
+    def seleccionar_muestra(self):  # -----> Metodo para seleccionar la muestra
+        pass
+
+    def graficar_muestra(self):  # -----> Metodo para graficar la muestra
+        pass
+
+    def colocar_organismo(self):  # -----> Metodo para colocar nuevos organismos
+        pass
+
+    def analizar_muestra(self):  # -----> Metodo para analizar la muestra
+        pass
+
+    def generar_xml(self):  # -----> Metodo para generar un archivo XML
+        pass
 
     def finalizar(self):  # -----> Metodo para finalizar el programa
         print('                                  ')
